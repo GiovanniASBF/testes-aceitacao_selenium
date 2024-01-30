5709a2499548dd7acd16e51867b72c2e49d1e4e6
Giovanni Bernardes Figueiredo <giovannibernardesfigueiredo@gmail.com>
2024-01-30 16:06:08 -0300

    Teste de cadastro de um novo leilao, parte 2. Finalizei o cadastro de um novo leilao. No processo foi adicionado um id na tabela leiloes.

--- src/test/java/br/com/alura/leilao/leiloes/LeiloesTest.java
+++ src/test/java/br/com/alura/leilao/leiloes/LeiloesTest.java
@@ -1,10 +1,14 @@
 package br.com.alura.leilao.leiloes;
 
 import br.com.alura.leilao.login.LoginPage;
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class LeiloesTest {
 
@@ -22,6 +26,13 @@ public class LeiloesTest {
         paginaDeLogin.preencheFormularioDeLogin("fulano", "pass");
         this.paginaDeLeiloes = paginaDeLogin.efetuaLogin();
         CadastroLeilaoPage paginaDeCadastro = paginaDeLeiloes.carregarFormulario();
+
+        String data = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        String nome = "Leilao do dia" + data;
+        String valor = "500.00";
+
+        this.paginaDeLeiloes = paginaDeCadastro.cadastrarLeilao(nome, valor, data);
+        Assert.assertTrue(paginaDeLeiloes.isLeilaoCadastrado(nome, valor, data));
     }
 
 }
